695f9d963b766aa7477ab2fe32ba2e57e1b94fa0
Magic.kch <magic.kch@yandex.ru>
2024-03-11 23:15:53 +0300

    add 1-15-The Longest Word_mod

--- STRINGS AND INTEGERS/1-15-The Longest Word.py
+++ STRINGS AND INTEGERS/1-15-The Longest Word.py
@@ -1,12 +1,5 @@
 def longest_word(sentence: str) -> str:
-    if sentence == "" or sentence == " ":
-        return ""
-    max_ = 0
-    for word in sentence.split():
-        if len(word) > max_:
-            max_ = len(word)
-            st = word
-    return st
+    return "" if sentence == "" or sentence == " " else max(sentence.split(), key=len)
 
 print("Example:")
 print(longest_word("hello world"))
