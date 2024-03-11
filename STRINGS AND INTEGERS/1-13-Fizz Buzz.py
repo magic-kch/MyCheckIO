62d28dd35964694337ed32383fb57d0f6d977406
Magic.kch <magic.kch@yandex.ru>
2024-03-11 22:54:16 +0300

    add 1-13-Fizz Buzz

---
+++ STRINGS AND INTEGERS/1-13-Fizz Buzz.py
@@ -0,0 +1,11 @@
+def checkio(num: int) -> str:
+    if num % 3 == 0 and num % 5 == 0:
+        return "Fizz Buzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    return str(num)
+
+print("Example:")
+print(checkio(15))
