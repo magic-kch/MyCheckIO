7ff1ad328aa6c9772f2ca1eb6a535284784c933f
Magic.kch <magic.kch@yandex.ru>
2024-03-14 23:13:19 +0300

    add 1-024-Beginning Zeros_mod

--- STRINGS AND INTEGERS/1-024-Beginning Zeros.py
+++ STRINGS AND INTEGERS/1-024-Beginning Zeros.py
@@ -1,11 +1,11 @@
 def beginning_zeros(a: str) -> int:
     sum_ = 0
-    if a[0] == "0":
-        for x in a:
-            if x != "0":
-                break
+    for x in a:
+        if x == "0":
             sum_ += 1
+        else:
+            break
     return sum_
 
 print("Example:")
-print(beginning_zeros("0010"))
+print(beginning_zeros("00000000000000010"))
