0ceba24ad7d225ddc4c6e0e9cfe0d6700343087b
Magic.kch <magic.kch@yandex.ru>
2024-03-15 01:10:43 +0300

    add 1-031-Sum of Integers_mod

--- STRINGS AND INTEGERS/1-031-Sum of Integers.py
+++ STRINGS AND INTEGERS/1-031-Sum of Integers.py
@@ -1,11 +1,5 @@
 def sum_upto_n(N: int) -> int:
-    sum = 1
-
-    while N > 1:
-        sum = sum + N
-        N = N - 1
-
-    return sum
+    return sum(x for x in range(N+1))
 
 print("Example:")
 print(sum_upto_n(10))
