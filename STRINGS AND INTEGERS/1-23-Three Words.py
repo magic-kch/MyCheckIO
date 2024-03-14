4f9b27facdee3a96e2477bb5ff16a10fa1364401
Magic.kch <magic.kch@yandex.ru>
2024-03-14 22:17:42 +0300

    add 1-23-Three Words_mod

--- STRINGS AND INTEGERS/1-23-Three Words.py
+++ STRINGS AND INTEGERS/1-23-Three Words.py
@@ -1,16 +1,8 @@
 def checkio(words: str) -> bool:
-    lst = words.split()
-    ok = 0
-    for word in lst:
-        try:
-            if int(word):
-                ok = 0
-        except ValueError:
-            ok += 1
-            if ok > 2:
-                return True
-
-    return False
+    for w in words.split():
+        if not w.isalpha():
+            return False
+    return True
 
 print("Example:")
-print(checkio("Hello World hello"))
+print(checkio("Hello World 1 hello"))
