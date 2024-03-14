48c94f0d88b71c72421666dcb723205593182e19
Magic.kch <magic.kch@yandex.ru>
2024-03-14 21:53:05 +0300

    add 1-20-Goes Right After (simplified)_mod

--- STRINGS AND INTEGERS/1-20-Goes Right After (simplified).py
+++ STRINGS AND INTEGERS/1-20-Goes Right After (simplified).py
@@ -1,7 +1,5 @@
 def goes_after(word: str, first: str, second: str) -> bool:
-    if word.find(first) == word.find(second)-1:
-        return True
-    return False
+    return True if word.find(first) == word.find(second)-1 else False
 
 print("Example:")
 print(goes_after("world", "w", "o"))
