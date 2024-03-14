f64364490c2e7244f4433fa4d5ca95a079cf24f7
Magic.kch <magic.kch@yandex.ru>
2024-03-14 23:18:37 +0300

    add 1-025-Count Substring Occurrences_mod

--- STRINGS AND INTEGERS/1-025-Count Substring Occurrences.py
+++ STRINGS AND INTEGERS/1-025-Count Substring Occurrences.py
@@ -1,9 +1,5 @@
 def count_occurrences(main_str: str, sub_str: str) -> int:
-    res = 0
-    for x in range(len(main_str)-len(sub_str)+1):
-        if main_str[x:len(sub_str)+x].lower() == sub_str.lower():
-            res += 1
-    return res
+    return sum(1 for x in range(len(main_str)-len(sub_str)+1) if main_str[x:len(sub_str)+x].lower() == sub_str.lower())
 
 print("Example:")
 print(count_occurrences("hello world hello", "hello"))
