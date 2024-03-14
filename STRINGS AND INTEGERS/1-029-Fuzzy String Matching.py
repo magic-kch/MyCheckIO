1881b46382f3296c5aa856b4bc32c649756a43fc
Magic.kch <magic.kch@yandex.ru>
2024-03-14 23:57:22 +0300

    add 1-029-Fuzzy String Matching_mod

--- STRINGS AND INTEGERS/1-029-Fuzzy String Matching.py
+++ STRINGS AND INTEGERS/1-029-Fuzzy String Matching.py
@@ -4,13 +4,7 @@ def fuzzy_string_match(str1: str, str2: str, threshold: int) -> bool:
     elif len(str2) > len(str1):
         str1 = str1.ljust(len(str2), "x")
 
-    sum = 0
-    for a in range(len(str1)):
-        if str1[a] != str2[a]:
-            sum += 1
-    if sum <= threshold:
-        return True
-    return False
+    return True if sum(1 for a in range(len(str1)) if str1[a] != str2[a]) <= threshold else False
 
 print("Example:")
 print(fuzzy_string_match("apple", "appel", 2))
