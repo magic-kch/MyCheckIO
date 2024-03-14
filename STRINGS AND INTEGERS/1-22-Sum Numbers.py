c66d1e2615da27456ba424c24d72da3032ed43d0
Magic.kch <magic.kch@yandex.ru>
2024-03-14 22:06:08 +0300

    add 1-22-Sum Numbers_mod_2

--- STRINGS AND INTEGERS/1-22-Sum Numbers.py
+++ STRINGS AND INTEGERS/1-22-Sum Numbers.py
@@ -1,9 +1,5 @@
 def sum_numbers(text: str) -> int:
-    sum_ = 0
-    for x in text.split():
-        if x.isdigit():
-            sum_ += int(x)
-    return sum_
+    return sum(int(x) for x in text.split() if x.isdigit())
 
 print("Example:")
 print(sum_numbers("This picture is an oil on canvas painting by Danish artist Anna Petersen between 1845 and 1910 year"))
