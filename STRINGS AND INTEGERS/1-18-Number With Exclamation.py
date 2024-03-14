4cf0e20d61fd0ad6c72f557c8653c81f7dc105b3
Magic.kch <magic.kch@yandex.ru>
2024-03-14 21:48:46 +0300

    add 1-18-Number With Exclamation_mod

--- STRINGS AND INTEGERS/1-18-Number With Exclamation.py
+++ STRINGS AND INTEGERS/1-18-Number With Exclamation.py
@@ -1,8 +1,5 @@
 def factorial(n: int) -> int:
-    if n == 1 or n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
+    return 1 if n == 1 or n == 0 else n * factorial(n - 1)
 
 print("Example:")
 print(factorial(4))
