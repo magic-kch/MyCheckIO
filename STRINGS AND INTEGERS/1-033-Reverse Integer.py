aaf235dad964b547722ff3b2cf2fa02830fd2121
Magic.kch <magic.kch@yandex.ru>
2024-03-15 11:00:41 +0300

    add 1-033-Reverse Integer_mod

--- STRINGS AND INTEGERS/1-033-Reverse Integer.py
+++ STRINGS AND INTEGERS/1-033-Reverse Integer.py
@@ -1,8 +1,5 @@
 def reverse_digits(num: int) -> int:
-    st = str(num)
-    if st[0] != "-":
-        return int(st[::-1])
-    return int("-" + st[:0:-1])
+    return int(str(num)[::-1]) if num >= 0 else -int(str(num)[:0:-1])
 
 print("Example:")
 print(reverse_digits(-32))
