f0543a0767be33745240f3121f2e75887fba70a9
Magic.kch <magic.kch@yandex.ru>
2024-03-15 11:12:50 +0300

    add 1-034-Armstrong Number Checking_mod1

--- STRINGS AND INTEGERS/1-034-Armstrong Number Checking.py
+++ STRINGS AND INTEGERS/1-034-Armstrong Number Checking.py
@@ -1,5 +1,5 @@
 def is_armstrong(num: int) -> bool:
-    return True if (sum(int(x) ** len(str(num)) for x in str(num))) == num else False
+    return sum(int(x) ** len(str(num)) for x in str(num)) == num
 
 print("Example:")
-print(is_armstrong(153))
+print(is_armstrong(103))
