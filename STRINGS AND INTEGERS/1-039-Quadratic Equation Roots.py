fc563cf81810556609c2ac147bbb6c6c56402223
Magic.kch <magic.kch@yandex.ru>
2024-03-16 11:59:00 +0300

    add 1-039-Quadratic Equation Roots_mod

--- STRINGS AND INTEGERS/1-039-Quadratic Equation Roots.py
+++ STRINGS AND INTEGERS/1-039-Quadratic Equation Roots.py
@@ -1,12 +1,6 @@
-def quadratic_roots(a: int, b: int, c: int)
-    D = b ** 2 - 4 * a * c
-    if D < 0:
-        return ["No real roots"]
-    elif D == 0:
-        return [-b / (2 * a), -b / (2 * a)]
-    elif D > 0:
-        D = D ** 0.5
-    return [(-b + D) / (2 * a), (-b - D) / (2 * a)]
+def quadratic_roots(a: int, b: int, c: int):
+    det = b ** 2 - 4 * a * c
+    return ["No real roots"] if det < 0 else [(-b + det ** 0.5) / (2 * a), (-b - det ** 0.5) / (2 * a)]
 
 print(quadratic_roots(1, -3, 2))
 print(quadratic_roots(1,2,3))
