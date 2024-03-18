9257888092d0e015edfe62e361418a363184b131
Magic.kch <magic.kch@yandex.ru>
2024-03-18 22:14:08 +0300

    add 1-046-Conversion into CamelCase_mod_2

--- STRINGS AND INTEGERS/1-046-Conversion into CamelCase.py
+++ STRINGS AND INTEGERS/1-046-Conversion into CamelCase.py
@@ -1,5 +1,5 @@
 def to_camel_case(name: str) -> str:
-    return "".join(x.title() for x in name.split("_"))
+    return name.title().replace("_","")
 
 print("Example:")
 print(to_camel_case("my_function_name"))
