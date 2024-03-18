94e41341b05748b250ce94ab9cea2144820e88e1
Magic.kch <magic.kch@yandex.ru>
2024-03-18 23:33:16 +0300

    add 2-001-Easy Unpack_mod

--- LIST BUT NOT THE LEAST/2-001-Easy Unpack.py
+++ LIST BUT NOT THE LEAST/2-001-Easy Unpack.py
@@ -1,9 +1,5 @@
 def easy_unpack(elements: tuple) -> tuple:
-    new_tup = []
-    new_tup.append(elements[0])
-    new_tup.append(elements[2])
-    new_tup.append(elements[-2])
-    return tuple(new_tup)
+    return tuple([elements[0], elements[2], elements[-2]])
 
 print("Example:")
 print(easy_unpack((1, 2, 3, 4, 5, 6, 7, 9)))
