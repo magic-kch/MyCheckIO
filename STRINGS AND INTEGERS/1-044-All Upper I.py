2b13a29cca4306fe82fa9065fd0ba627247e17b6
Magic.kch <magic.kch@yandex.ru>
2024-03-18 21:51:22 +0300

    add 1-044-All Upper I_mod

--- STRINGS AND INTEGERS/1-044-All Upper I.py
+++ STRINGS AND INTEGERS/1-044-All Upper I.py
@@ -1,8 +1,12 @@
 def is_all_upper(text: str) -> bool:
-    for char in text:
-        if char.islower():
-            return False
-    return True
+    return text == text.upper()
 
 print("Example:")
 print(is_all_upper("ALL UPPER"))
+
+print(is_all_upper("ALL UPPER")," == True")
+print(is_all_upper("all lower")," == False")
+print(is_all_upper("mixed UPPER and lower")," == False")
+print(is_all_upper("")," == True")
+print(is_all_upper("444")," == True")
+print(is_all_upper("55 55 5 ")," == True")
