43716507322b8ffd9fa32768ffafa9637cce3330
Magic.kch <magic.kch@yandex.ru>
2024-03-18 22:05:13 +0300

    add 1-045-Conversion from CamelCase_mod

--- STRINGS AND INTEGERS/1-045-Conversion from CamelCase.py
+++ STRINGS AND INTEGERS/1-045-Conversion from CamelCase.py
@@ -1,14 +1,11 @@
 def from_camel_case(name: str) -> str:
     lst = []
-    begin_str = 0
-    end_str = 0
-    while end_str < len(name) - 1:
-        end_str += 1
-        if name[end_str].isupper():
-            lst.append(name[begin_str:end_str].lower())
-            begin_str = end_str
-    lst.append(name[begin_str:end_str + 1].lower())
-    return "_".join(lst)
+    for c in name:
+        if c.isupper():
+            lst.append("_" + c.lower())
+        else:
+            lst.append(c)
+    return "".join(lst)[1:]
 
 print("Example:")
 print(from_camel_case("MyFunctionName"))
