1bdd887023d18fc652d03e03dc7c65c354f658c6
Magic.kch <magic.kch@yandex.ru>
2024-03-19 00:07:23 +0300

    add 2-004-Even the Last_mod_2

--- LIST BUT NOT THE LEAST/2-004-Even the Last.py
+++ LIST BUT NOT THE LEAST/2-004-Even the Last.py
@@ -1,5 +1,5 @@
 def checkio(array: list[int]) -> int:
-    return 0 if len(array) == 0 else sum(array[i] for i in range(0, len(array), 2)) * array[-1]
+    return 0 if len(array) == 0 else sum(array[0::2]) * array[-1]
 
 print("Example:")
 print(checkio([0, 1, 2, 3, 4, 5]))
