497d74baf40ea925029be01ad69847579fc275ba
Magic.kch <magic.kch@yandex.ru>
2024-03-19 15:53:58 +0300

    add 2-014-Compress List_mod

--- LIST BUT NOT THE LEAST/2-014-Compress List.py
+++ LIST BUT NOT THE LEAST/2-014-Compress List.py
@@ -2,13 +2,7 @@ from collections.abc import Iterable
 
 
 def compress(items: list[int]) -> Iterable[int]:
-    if len(items) < 1:
-        return items
-    new = [items[0],]
-    for i in range(1, len(items)):
-        if new[-1] != items[i]:
-            new.append(items[i])
-    return new
+    return items if len(items) < 1 else [items[0]] + [items[i] for i in range(1, len(items)) if items[i-1] != items[i]]
 
 print(compress([5, 5, 5, 4, 5, 6, 6, 5, 5, 7, 8, 0, 0]))
 print(compress([5]))
