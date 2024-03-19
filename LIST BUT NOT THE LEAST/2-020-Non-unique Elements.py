595a829a9610024c447e82fa11b790b1f0a0c34e
Magic.kch <magic.kch@yandex.ru>
2024-03-19 20:33:34 +0300

    add 2-020-Non-unique Elements_mod

--- LIST BUT NOT THE LEAST/2-020-Non-unique Elements.py
+++ LIST BUT NOT THE LEAST/2-020-Non-unique Elements.py
@@ -1,12 +1,5 @@
-from collections.abc import Iterable
-
-
-def checkio(data: list[int]) -> Iterable[int]:
-    new_data = []
-    for i in data:
-        if data.count(i) > 1:
-            new_data.append(i)
-    return new_data
+def checkio(data: list[int]):
+    return [i for i in data if data.count(i) > 1]
 
 print("Example:")
 print(list(checkio([1, 2, 3, 1, 3])))
