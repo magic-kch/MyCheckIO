ec058d2394f5c53b36f360ae0298af467d37e6f1
Magic.kch <magic.kch@yandex.ru>
2024-03-19 16:03:54 +0300

    add 2-015-Remove All After_mod

--- LIST BUT NOT THE LEAST/2-015-Remove All After.py
+++ LIST BUT NOT THE LEAST/2-015-Remove All After.py
@@ -1,5 +1,5 @@
 def remove_all_after(items: list[int], border: int):
-    return items[0:items.index(border)+1] if border in items else items
+    return items[:items.index(border)+1] if border in items else items
 
 print("Example:")
 print(list(remove_all_after([1, 2, 3, 4, 5], 3)))
