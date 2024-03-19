ae9f97c3d3cf962e4e61fbdf1a751d18898bed52
Magic.kch <magic.kch@yandex.ru>
2024-03-19 23:14:45 +0300

    add 2-011-Backward Each Word_mod

--- LIST BUT NOT THE LEAST/2-011-Backward Each Word.py
+++ LIST BUT NOT THE LEAST/2-011-Backward Each Word.py
@@ -1,10 +1,5 @@
 def backward_string_by_word(text: str) -> str:
-    if not text:
-        return text
-    ntext = []
-    for x in text.split(" "):
-        ntext.append(x[::-1])
-    return " ".join(ntext)
+    return text if not text else " ".join(x[::-1] for x in text.split(" "))
 
 print("Example:")
 print(backward_string_by_word("olleH Hello"))
