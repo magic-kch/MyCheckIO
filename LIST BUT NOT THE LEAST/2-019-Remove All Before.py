955ffd60451af3e61557c34d6115c77362efca99
Magic.kch <magic.kch@yandex.ru>
2024-03-19 19:11:17 +0300

    add 2-019-Remove All Before_mod

--- LIST BUT NOT THE LEAST/2-019-Remove All Before.py
+++ LIST BUT NOT THE LEAST/2-019-Remove All Before.py
@@ -1,10 +1,5 @@
-from collections.abc import Iterable
-
-
-def remove_all_before(items: list, border: int) -> Iterable:
-    if border in items:
-        return items[items.index(border)::]
-    return items
+def remove_all_before(items: list, border: int):
+    return items[items.index(border):] if border in items else items
 
 print("Example:")
 print(list(remove_all_before([1, 2, 3, 4, 5], 3)))
