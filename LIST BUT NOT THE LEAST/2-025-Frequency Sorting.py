408495dda4add576ee1da0c4b0bcc60082aac6ca
Magic.kch <magic.kch@yandex.ru>
2024-03-19 22:17:39 +0300

    add 2-025-Frequency Sorting_mod

--- LIST BUT NOT THE LEAST/2-025-Frequency Sorting.py
+++ LIST BUT NOT THE LEAST/2-025-Frequency Sorting.py
@@ -2,15 +2,7 @@ from collections.abc import Iterable
 
 
 def frequency_sorting(numbers: list[int]) -> Iterable[int]:
-    n_set = sorted(list(set(numbers)))
-    n_dict = {}
-    res = []
-    for i in n_set:
-        n_dict[i] = numbers.count(i)
-    n_dict_sort = dict(sorted(n_dict.items(), key=lambda item: item[1], reverse=True))
-    for key, val in n_dict_sort.items():
-        res.extend([key] * val)
-    return res
+    return sorted(sorted(numbers), key=numbers.count, reverse=True)
 
 print("Example:")
 print(list(frequency_sorting([1, 2, 3, 4, 5])))
