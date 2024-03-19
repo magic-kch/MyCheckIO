1036a5c72a99e011b8de396413e727dcb50c3d6a
Magic.kch <magic.kch@yandex.ru>
2024-03-19 10:58:00 +0300

    add 2-009-All the Same_mod

--- LIST BUT NOT THE LEAST/2-009-All the Same.py
+++ LIST BUT NOT THE LEAST/2-009-All the Same.py
@@ -2,13 +2,10 @@ from typing import List, Any
 
 
 def all_the_same(elements: List[Any]) -> bool:
-    if len(elements) <= 1:
-        return True
-    for i in range(len(elements)):
-        if elements[i] != elements[i-1]:
-            return False
-    return True
+    return len(elements) <= 1 or elements.count(elements[0]) == len(elements)
 
 print("Example:")
 print(all_the_same([1, 1, 1]))
 print(all_the_same([1, "a", 1]))
+print(all_the_same([1]))
+print(all_the_same([]))
