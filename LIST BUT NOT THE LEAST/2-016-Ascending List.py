f9be9b26817884c1b57237b41205f18da566d978
Magic.kch <magic.kch@yandex.ru>
2024-03-19 16:15:38 +0300

    add 2-016-Ascending List_mod

--- LIST BUT NOT THE LEAST/2-016-Ascending List.py
+++ LIST BUT NOT THE LEAST/2-016-Ascending List.py
@@ -1,8 +1,5 @@
 def is_ascending(items: list[int]) -> bool:
-    for i in range(len(items)-1):
-        if items[i] >= items[i+1]:
-            return False
-    return True
+    return sorted(set(items)) == items
 
 print("Example:")
-print(is_ascending([-5, 10, 99, 123456]))
+print(is_ascending([-5, 10, 1, 99, 123456]))
